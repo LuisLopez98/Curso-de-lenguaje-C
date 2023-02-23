612f34b3cb1cbea29f353505ca1fe8e5f43d4d00
LuisLopez98 <111952650+LuisLopez98@users.noreply.github.com>
2023-02-23 14:10:45 -0600

    Tipos de datos
    Se muestran los tipos de datos y como poner las palabra que lo soportan (char, int, double, float, etc...)

---
+++ Tipos de datos.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int numero = 10; //sirve para almacenar numeros enteros
+	float numero_decimal = 2.7;
+	double numero_decima2= 2.7;//permite definir mas decimales que float
+	char letra = 'J'; // char de caracter y se ponen entre comill a simple
+	char palabra [] = "tengo sueño"; // los corchetes se ponen porque este es una reglo de caracteres.
+	
+	return 0;
+}
